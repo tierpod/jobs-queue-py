3c7e7b9e2c37a307493c3bdf874eb63253499073
Pavel Podkorytov <pod.pavel@gmail.com>
2018-08-22 13:39:24 +0500

    Add cache with different delete modes

--- bin/jobs-queue.py
+++ bin/jobs-queue.py
@@ -13,10 +13,14 @@ import subprocess
 import threading
 from SocketServer import UnixDatagramServer, DatagramRequestHandler
 
-from cachetools import TTLCache
+import cachetools
 
 
 DEFAULT_CFG_PATH = "/etc/jobs-queue.ini"
+
+CACHE_DELETE_EXPIRE = "expire"
+CACHE_DELETE_COMPLETE = "complete"
+CACHE_DELETE_EXPIRE_COMPLETE = "expire_complete"
 
 
 def parse_args():
@@ -53,7 +57,6 @@ class RequestHandler(DatagramRequestHandler):
             return
 
         try:
-            cache[data] = None
             queue.put(data, block=False, timeout=1)
         except Queue.Full:
             log.error("queue limit is exceeded")
@@ -70,17 +73,20 @@ class RequestHandler(DatagramRequestHandler):
 
 
 class Worker(threading.Thread):
-    def __init__(self, name, queue):
+    def __init__(self, name, queue, cache):
         super(Worker, self).__init__(name=name)
         self.daemon = True
 
         log.info("start worker %s", self.name)
         self.queue = queue
+        self.cache = cache
 
     def run(self):
         while True:
             data = self.queue.get()
+            self.cache[data] = None
             self.process(data)
+            del self.cache[data]
 
     def process(self, data):
         log.info("exec  : %s", data)
@@ -100,6 +106,36 @@ class Worker(threading.Thread):
             log.error("exec  : %s failed with %d", data, p.returncode)
 
 
+class Cache(object):
+    def __init__(self, mode, maxsize, ttl):
+        self.mode = mode
+        if mode == CACHE_DELETE_COMPLETE:
+            self.cache = cachetools.Cache(maxsize=maxsize)
+        else:
+            self.cache = cachetools.TTLCache(maxsize=maxsize, ttl=ttl)
+
+    def __str__(self):
+        return str(self.cache)
+
+    def __delitem__(self, key):
+        if self.mode == CACHE_DELETE_EXPIRE:
+            return
+
+        try:
+            self.cache.__delitem__(key)
+        except KeyError:
+            return
+
+    def __setitem__(self, key, value):
+        self.cache.__setitem__(key, value)
+
+    def __getitem__(self, key):
+        return self.cache.__getitem__(key)
+
+    def __contains__(self, key):
+        return self.cache.__contains__(key)
+
+
 class Config(object):
     def __init__(self, socket, workers, log_debug, log_datetime, queue_size, cache_delete_mode,
                  cache_expire):
@@ -115,13 +151,18 @@ class Config(object):
     def from_file(cls, path):
         c = ConfigParser.ConfigParser()
         c.read(path)
+        cache_delete_mode = c.get("main", "cache_delete_mode")
+        if cache_delete_mode not in (CACHE_DELETE_EXPIRE, CACHE_DELETE_COMPLETE,
+                                     CACHE_DELETE_EXPIRE_COMPLETE):
+            raise ValueError("wrong cache_delete_mode value")
+
         return cls(
             socket=c.get("main", "socket"),
             workers=c.getint("main", "workers"),
             log_debug=c.getboolean("main", "log_debug"),
             log_datetime=c.getboolean("main", "log_datetime"),
             queue_size=c.getint("main", "queue_size"),
-            cache_delete_mode=c.get("main", "cache_delete_mode"),
+            cache_delete_mode=cache_delete_mode,
             cache_expire=c.getint("main", "cache_expire"),
         )
 
@@ -136,11 +177,11 @@ if __name__ == "__main__":
 
     signal.signal(signal.SIGTERM, signal_handler)
 
-    cache = TTLCache(maxsize=config.queue_size, ttl=config.cache_expire)
+    cache = Cache(mode=config.cache_delete_mode, maxsize=config.queue_size, ttl=config.cache_expire)
 
     queue = Queue.Queue(maxsize=config.queue_size)
     for i in range(1, config.workers + 1):
-        worker = Worker(name="Worker-%d" % i, queue=queue)
+        worker = Worker(name="Worker-%d" % i, queue=queue, cache=cache)
         worker.start()
 
     log.info("listen unix socket on %s", config.socket)
